3f17b34080b81118c76bf462dfccd375555ab160
Alibek Kaliyev <alk224@lehigh.edu>
2021-01-24 12:23:41 -0500

    Added functions

--- codes/util/preprocessing.py
+++ codes/util/preprocessing.py
@@ -26,35 +26,3 @@ class my_scaler:
         data *= self.std
         data += self.mean
         return data
-
-
-class Transformer:
-    def __init__(self, params_real, params_pred):
-        self.params_real = params_real
-        self.params_pred = params_pred
-
-    def transform_params(params_real, params_pred):
-        params_pred[:, 3] = params_pred[:, 3] + 1/2 * np.pi
-        params_real[:, 3] = params_real[:, 3] + 1/2 * np.pi
-
-        params_pred[:, 3] = np.where(
-            params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
-
-        params_pred[:, 3] = np.where(
-            params_pred[:, 2] > 0, params_pred[:, 3], params_pred[:, 3] + np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 2] > 0, params_real[:, 3], params_real[:, 3] + np.pi)
-
-        params_pred[:, 2] = np.where(
-            params_pred[:, 2] > 0, params_pred[:, 2], params_pred[:, 2]*-1)
-        params_real[:, 2] = np.where(
-            params_real[:, 2] > 0, params_real[:, 2], params_real[:, 2]*-1)
-
-        params_pred[:, 3] = np.where(
-            params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
-
-        return params_real, params_pred
