e551ac1a8479d70b99e4c1e388f4ddc96b10655c
Alibek Kaliyev <alk224@lehigh.edu>
2021-01-24 13:05:18 -0500

    Update postprocessing.py

--- codes/util/postprocessing.py
+++ codes/util/postprocessing.py
@@ -1,34 +1,28 @@
 import numpy as np
 
 
-class Transformer:
+def transform_params(params_real, params_pred):
+    params_pred[:, 3] = params_pred[:, 3] + 1/2 * np.pi
+    params_real[:, 3] = params_real[:, 3] + 1/2 * np.pi
 
-    def __init__(self, params_real, params_pred):
-        self.params_real = params_real
-        self.params_pred = params_pred
+    params_pred[:, 3] = np.where(
+        params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
+    params_real[:, 3] = np.where(
+        params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
 
-    def transform_params(params_real, params_pred):
-        params_pred[:, 3] = params_pred[:, 3] + 1/2 * np.pi
-        params_real[:, 3] = params_real[:, 3] + 1/2 * np.pi
+    params_pred[:, 3] = np.where(
+        params_pred[:, 2] > 0, params_pred[:, 3], params_pred[:, 3] + np.pi)
+    params_real[:, 3] = np.where(
+        params_real[:, 2] > 0, params_real[:, 3], params_real[:, 3] + np.pi)
 
-        params_pred[:, 3] = np.where(
-            params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
+    params_pred[:, 2] = np.where(
+        params_pred[:, 2] > 0, params_pred[:, 2], params_pred[:, 2]*-1)
+    params_real[:, 2] = np.where(
+        params_real[:, 2] > 0, params_real[:, 2], params_real[:, 2]*-1)
 
-        params_pred[:, 3] = np.where(
-            params_pred[:, 2] > 0, params_pred[:, 3], params_pred[:, 3] + np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 2] > 0, params_real[:, 3], params_real[:, 3] + np.pi)
+    params_pred[:, 3] = np.where(
+        params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
+    params_real[:, 3] = np.where(
+        params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
 
-        params_pred[:, 2] = np.where(
-            params_pred[:, 2] > 0, params_pred[:, 2], params_pred[:, 2]*-1)
-        params_real[:, 2] = np.where(
-            params_real[:, 2] > 0, params_real[:, 2], params_real[:, 2]*-1)
-
-        params_pred[:, 3] = np.where(
-            params_pred[:, 3] < np.pi, params_pred[:, 3], params_pred[:, 3] - 2 * np.pi)
-        params_real[:, 3] = np.where(
-            params_real[:, 3] < np.pi, params_real[:, 3], params_real[:, 3] - 2 * np.pi)
-
-        return params_real, params_pred
+    return params_real, params_pred
